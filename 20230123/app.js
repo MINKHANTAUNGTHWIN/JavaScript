73299ebdbbadb2a7c61c0fd9c04d52d3e6f32bb2
MINKHANTAUNGTHWIN <w22003@osfl.ac.jp>
2023-01-24 09:53:59 +0900

    20230123 JS Learning

---
+++ 20230123/app.js
@@ -0,0 +1,22 @@
+// variable declaration
+
+let a = 10;
+let b = a + 20;
+
+
+a += 20;
+
+a++;
+
+console.log(a);
+
+console.log(a++);
+console.log(++a);
+
+let c = 0;
+let d = 0;
+
+let e = (--c) + (--d) + (c++) + (d++);
+console.log(e);
+console.log(c);
+console.log(d);
